e6bf522b0bab45a98ecd3e112fc2bf700f09fac7
First123vb <chamanjayaram.lrf20@rvce.edu.in>
2022-01-06 16:50:31 +0530

    Added Hello World project files

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(int argc, char* argv[]) {
+   // printf() displays the string inside quotation
+	if (argc > 1)
+		return 1;
+
+   printf("Hello, World!");
+   return 0;
+}
